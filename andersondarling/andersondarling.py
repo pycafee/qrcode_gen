ddca99e4fda2b74bfd752708951fd9195c3c6e20
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2022-07-12 23:47:31 -0300

    add qrcodes
    QR codes were generated to shapirowilk, anderson darling and lilliefors posts

---
+++ andersondarling/andersondarling.py
@@ -0,0 +1,31 @@
+import qrcode
+
+
+# -------------- FUNCTION/CLASS -------------- #
+name = "andersondarling"
+language = "pt-br"
+baptism_of_fire = name + "-" + language + ".png"
+
+# -------------- ORIGINAL LINK -------------- #
+original_link = "https://colab.research.google.com/drive/1wAH9Sa6sdsDcYNEPNKXO3AuNox3qJEm4?usp=sharing"
+
+# -------------- bitly LINK -------------- #
+bitly_link = "https://bit.ly/anderson_darling"
+
+# -------------- Creating inbstance for qrcode -------------- #
+qr = qrcode.QRCode(
+    version=None,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# -------------- Adding the link -------------- #
+qr.add_data(bitly_link)
+
+# -------------- Making the qrcode -------------- #
+qr.make(fit=True)
+
+# -------------- Exporting the qrcode -------------- #
+img = qr.make_image(fill_color="black", back_color="white")
+img.save(baptism_of_fire)
---
+++ shapirowilk/shapirowilk.py
@@ -0,0 +1,31 @@
+import qrcode
+
+
+# -------------- FUNCTION/CLASS -------------- #
+name = "shapirowilk"
+language = "pt-br"
+baptism_of_fire = name + "-" + language + ".png"
+
+# -------------- ORIGINAL LINK -------------- #
+original_link = "https://colab.research.google.com/drive/1KUP0YuJLtoVekzB8HhtMzmAd2e3nJUsP?usp=sharing"
+
+# -------------- bitly LINK -------------- #
+bitly_link = "https://bit.ly/shapiro_wilk"
+
+# -------------- Creating inbstance for qrcode -------------- #
+qr = qrcode.QRCode(
+    version=None,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# -------------- Adding the link -------------- #
+qr.add_data(bitly_link)
+
+# -------------- Making the qrcode -------------- #
+qr.make(fit=True)
+
+# -------------- Exporting the qrcode -------------- #
+img = qr.make_image(fill_color="black", back_color="white")
+img.save(baptism_of_fire)
